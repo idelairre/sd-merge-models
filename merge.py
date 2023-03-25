2f6dd39753ff7fb49ac06f68d5d9da187cbe3731
Ian Delairre <idelairre@gmail.com>
2023-03-25 18:20:52 -0400

    adds various merge types, sigmoid merge is now actually a sigmoid merge, although whether or not its useful is a real question.

--- merge.py
+++ merge.py
@@ -54,31 +54,74 @@ def get_state_dict_from_checkpoint(pl_sd):
     return pl_sd
 
 
-def merge(answers, path="C:\\Users\\idela\\Documents\\Github\\stable-diffusion-webui\\models\\Stable-diffusion\\"):
-    model0 = path + answers.get("model1")
-    model1 = path + answers.get("model2")
-    device = answers.get("device")
-    without_vae = answers.get("without_vae") == "yes"
-    alpha = float(answers.get("alpha"))
-    output = path + answers.get("output")
-    safetensor = answers.get("safetensor") == "yes"
+def merge(model1, model2, model3, alpha, without_vae, device, output, safetensor, merge_type):
+    path="C:\\Users\\idela\\Documents\\Github\\stable-diffusion-webui\\models\\Stable-diffusion\\"
+    model0 = path + model1
+    model1 = path + model2
+    if model3:
+        model2 = path + model3
 
     theta_0 = read_state_dict(model0, map_location=device)
     theta_1 = read_state_dict(model1, map_location=device)
+
+    if model3:
+        theta_2 = read_state_dict(model2, map_location=device)
 
     output_file = f"{output}-{str(alpha)[2:] + '0'}.ckpt"
 
     if safetensor:
         output_file = f"{output_file.replace('.ckpt', '')}.safetensors"
 
-    for key in tqdm(theta_0.keys(), desc="Stage 1/2"):
-        # skip VAE model parameters to get better results(tested for anime models)
-        # for anime model，with merging VAE model, the result will be worse (dark and blurry)
-        if without_vae and "first_stage_model" in key:
-            continue
+    if merge_type == "Sigmoid":
+        for key in tqdm(theta_0.keys(), desc="Stage 1/2"):
+            if without_vae and "first_stage_model" in key:
+                continue
 
-        if "model" in key and key in theta_1:
-            theta_0[key] = (1 - alpha) * theta_0[key] + alpha * theta_1[key]
+            if "model" in key and key in theta_1:
+                theta_0[key] = (1 / (1 + torch.exp(-4 * alpha))) * (theta_0[key] + theta_1[key]) - \
+                    (1 / (1 + torch.exp(-alpha))) * theta_0[key]
+
+    elif merge_type == "Linear":
+        for key in tqdm(theta_0.keys(), desc="Stage 1/2"):
+            if without_vae and "first_stage_model" in key:
+                continue
+
+            if "model" in key and key in theta_1:
+                theta_0[key] = (1 - alpha) * theta_0[key] + \
+                    alpha * theta_1[key]
+
+    elif merge_type == "Geometric":
+        for key in tqdm(theta_0.keys(), desc="Stage 1/2"):
+            if without_vae and "first_stage_model" in key:
+                continue
+
+            if "model" in key and key in theta_1:
+                theta_0[key] = torch.pow(
+                    theta_0[key], 1 - alpha) * torch.pow(theta_1[key], alpha)
+
+    elif merge_type == "Max":
+        for key in tqdm(theta_0.keys(), desc="Stage 1/2"):
+            if without_vae and "first_stage_model" in key:
+                continue
+
+            if "model" in key and key in theta_1:
+                theta_0[key] = torch.max(theta_0[key], theta_1[key])
+
+    elif merge_type == "Difference":
+        if not model3:
+            raise ValueError(
+                "A third model must be provided for Difference merge")
+
+        for key in tqdm(theta_0.keys(), desc="Stage 1/3"):
+            if without_vae and "first_stage_model" in key:
+                continue
+
+            if "model" in key and key in theta_1 and key in theta_2:
+                theta_0[key] = theta_0[key] + \
+                    (theta_1[key] - theta_2[key]) * alpha
+
+    else:
+        raise ValueError("Invalid merge type selected")
 
     for key in tqdm(theta_1.keys(), desc="Stage 2/2"):
         if "model" in key and key not in theta_0:
@@ -95,26 +138,6 @@ def merge(answers, path="C:\\Users\\idela\\Documents\\Github\\stable-diffusion-w
     print("Done!")
 
 
-def save_answers(answers):
-    with open('options.json', 'w', encoding='utf8') as json_file:
-        json.dump(answers, json_file, ensure_ascii=True)
-
-
-def merge_and_save(model1, model2, alpha, without_vae, device, output, safetensor):
-    answers = {
-        "model1": model1,
-        "model2": model2,
-        "alpha": alpha,
-        "without_vae": without_vae,
-        "device": device,
-        "output": output,
-        "safetensor": safetensor,
-    }
-
-    merge(answers)
-    return "Done! Models merged and saved."
-
-
 def update_model_list(path):
     global model_list
     model_list = [f for f in os.listdir(path) if f.endswith(
@@ -123,14 +146,11 @@ def update_model_list(path):
 
 
 def app():
-    # file_path = gr.inputs.Textbox(
-    #     default="C:\\Users\\idela\\Documents\\Github\\stable-diffusion-webui\\models\\Stable-diffusion\\",
-    #     label="Enter the file path",
-    # )
     model_list = update_model_list(
         "C:\\Users\\idela\\Documents\\Github\\stable-diffusion-webui\\models\\Stable-diffusion\\")
     model1 = gr.inputs.Dropdown(model_list, label="Select the first model")
     model2 = gr.inputs.Dropdown(model_list, label="Select the second model")
+    model3 = gr.inputs.Dropdown(model_list, label="Select the third model")
     alpha = gr.inputs.Slider(minimum=0, maximum=1,
                              step=0.01, default=0.5, label="Alpha value")
     without_vae = gr.inputs.Radio(
@@ -141,11 +161,13 @@ def app():
         default="merged", label="Output file name, without extension")
     safetensor = gr.inputs.Radio(
         ["yes", "no"], label="Convert to safetensor format", default="yes")
+    merge_type = gr.inputs.Dropdown(
+        ["Sigmoid", "Linear", "Geometric", "Max", "Difference"], label="Select merge type")
 
     iface = gr.Interface(
-        fn=merge_and_save,
-        inputs=[model1, model2, alpha, without_vae,
-                device, output, safetensor],
+        fn=merge,
+        inputs=[model1, model2, model3, alpha, without_vae,
+                device, output, safetensor, merge_type],
         outputs="text",
         title="Model Merger",
         description="Merge two models with the given parameters.",
